52fd79d682f500a1d13ffca3824a68d899a7ba0d
idrmiller <idrmiller360@gmail.com>
2016-01-18 16:20:47 -0500

    Completed corrections on X selection., Changed play again position to button of screen, was more user-friendly.

--- iOS_Rock_Paper_Scissors/SuccessViewController.swift
+++ iOS_Rock_Paper_Scissors/SuccessViewController.swift
@@ -37,16 +37,23 @@ class SuccessViewController: UIViewController {
         UIView.animateWithDuration(0.3) {
             self.userImage.alpha = 1.0
             self.computerImage.alpha = 1.0
+            self.userPlayed.alpha = 1.0
+            self.computerPlayed.alpha = 1.0
         }
         
         UIView.animateWithDuration(0.5) {
             self.winOrLooseMessage.alpha = 1.0
             
-            if self.userLoose.hidden == false {
+            if self.userLoose.hidden == true && self.computerLoose.hidden == true{
+                self.userLoose.alpha = 0
+                self.computerLoose.alpha = 0
+            } else if self.computerLoose.hidden == true {
                 self.userLoose.alpha = 1.0
-            } else {
+            } else if self.userLoose.hidden == true {
                 self.computerLoose.alpha = 1.0
             }
+            
+            
             
         }
         
@@ -64,6 +71,23 @@ class SuccessViewController: UIViewController {
         self.computerImage.image = UIImage(named: "item\(computerInput!)")
         
         
+        //Set label for played item
+        func setLabel(input: Int) -> String{
+            switch input {
+            case 1:
+                return "Played Rock"
+            case 2:
+                return "Played Paper"
+            case 3:
+                return "Played Scissors"
+            default:
+                return "Played Item"
+            }
+        }
+        
+        userPlayed.text = setLabel(userInput!)
+        computerPlayed.text = setLabel(computerInput!)
+        
         
         //Calculate winner and set lables
         //TODO: Rock beats scissors (R = 1 vs S = 3)
@@ -73,26 +97,29 @@ class SuccessViewController: UIViewController {
         //TODO: All other cases user looses
         if userInput == 1 && computerInput == 3{
             winOrLooseMessage.text = "You Win, Rock crushes Scissors!"
-            computerLoose.hidden = false
+            userLoose.hidden = true
         } else if userInput == 2 && computerInput == 1 {
             winOrLooseMessage.text = "You Win, Paper covers Rock!"
-            computerLoose.hidden = false
+            userLoose.hidden = true
         } else if userInput == 3 && computerInput == 2 {
             winOrLooseMessage.text = "You Win, Scissor cuts Paper!"
-            computerLoose.hidden = false
+            userLoose.hidden = true
         } else if userInput == computerInput {
             winOrLooseMessage.text = "TIE"
+            userLoose.hidden = true
+            computerLoose.hidden = true
         } else {
             winOrLooseMessage.text = "You Lost"
-            userLoose.hidden = false
+            computerLoose.hidden = true
         }
         
         self.userImage.alpha = 0
         self.computerImage.alpha = 0
         self.userLoose.alpha = 0
+        self.userPlayed.alpha = 0
+        self.computerPlayed.alpha = 0
         self.computerLoose.alpha = 0
         self.winOrLooseMessage.alpha = 0
-        
         
         
     }
